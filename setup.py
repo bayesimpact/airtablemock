0ad7631bb17316972d69d3428dd9fac6d3727dea
Pascal Corpet <pcorpet@users.noreply.github.com>
2017-11-16 10:09:13 +0100

    Release v0.0.2 with the fix. (#3)

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
+
 setup(
     name='airtablemock',
-    version='0.0.1',
+    version='0.0.2',
     packages=['airtablemock'],
     install_requires=['mock>=2.0.0'],
     description='Mock library for the airtable client',
