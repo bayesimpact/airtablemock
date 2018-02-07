72f9a9eca0af28e269ce1aa06eb407cbbf66e54f
Pascal Corpet <pcorpet@users.noreply.github.com>
2018-02-07 14:55:43 +0100

    Add a TestCase class to ease independant unit testing. Also release v0.0.3. (#6)

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(
     name='airtablemock',
-    version='0.0.2',
+    version='0.0.3',
     packages=['airtablemock'],
     install_requires=['mock>=2.0.0'],
+    tests_require=['airtable', 'pytest'],
+    setup_requires=['pytest-runner'],
     description='Mock library for the airtable client',
     author='Pascal Corpet',
     url='https://github.com/bayesimpact/airtablemock',
